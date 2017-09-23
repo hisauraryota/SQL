e278d94d6bc4aab332d75ffaaa9b32483adf085e
RYOTA-PC\ryota <ryota.hisaura39@gmail.com>
2017-09-23 11:18:05 +0900

    問9まで完了

--- Q1.sql
+++ Q1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE item_category (user_id int PRIMARY KEY AUTO_INCREMENT, category_name varchar(256) NOT NULL);
+CREATE TABLE item_category (category_id int PRIMARY KEY AUTO_INCREMENT, category_name varchar(256) NOT NULL);
---
+++ Q9.sql
@@ -0,0 +1,1 @@
+SELECT category_name, SUM(item_price) AS total_price FROM item INNER JOIN item_category ON item.category_id = item_category.category_id GROUP BY item_category.category_id  ORDER BY total_price DESC;
